e5c32c8cf46bffd9740f94816dc76ad3973e339f
Elise Olivares <elisejpo@gmail.com>
2018-01-31 22:55:35 -0800

    posts, comments, categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(str)
+    str.starts_with?("http://") ? str : "https://#{str}"
+  end
+
+  def display_datetime(dt)
+    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201020145) do
+ActiveRecord::Schema.define(version: 20180201022358) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "user_id"
     t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
